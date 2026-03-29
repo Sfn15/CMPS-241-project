c1d5bebb202232de4edbe0b82b62a766bacc321f
Sfn15 <sfn15@mail.aub.edu>
2026-03-29 03:04:55 +0300

    added player move input, which adds the lines. remains to add the characters for owned squares and to update the score

--- helper.c
+++ helper.c
@@ -1,11 +1,97 @@
 #include "helper.h"
 #include <stdio.h>
 
-void playerMove(char p, char* vlines[HEIGHT][LENGTH+1], char* hlines[HEIGHT+1][LENGTH]){
+int validateInput(int (*move)[4]){
+    // return -1 for invalid input
+    // return 0 for horizontal line
+    // return 1 for vertical line
+    // also order the input so that the logic is easier
+    int *m = *move;
+    int temp;
+
+    for(int i = 0; i < 2; i++){ //check for out of bounds input
+        if(m[2*i] > HEIGHT || m[2*i] < 0){
+            return -1;
+        }
+        if(m[2*i+1]> LENGTH || m[2*i+1] < 0){
+            return -1;
+        }
+    }
+    if(m[0]==m[2]){ // if the input is horizontal
+        if(m[1]-m[3] == 1 || m[3] - m[1] == 1){ //check the others are exactly one apart
+            if(m[1] > m[3]){
+                temp = m[3];
+                m[3] = m[1];
+                m[1] = temp;
+            }
+            return 0;
+        } else {
+            return -1;
+        }
+    } else if (m[1] == m[3]){ //if the input is vertical
+        if(m[0]-m[2] == 1 || m[2] - m[0] == 1){ //check the others are exactly one apart
+            if(m[0] > m[2]){
+                temp = m[2];
+                m[2] = m[0];
+                m[0] = temp;
+            }
+            return 1;
+        } else {
+            return -1;
+        }
+    } else {
+        return -1;
+    }
+}
+
+
+// return 1 if failed and 0 if successful.
+int playerMove(char p, char vlines[HEIGHT][LENGTH+1], char hlines[HEIGHT+1][LENGTH], int coord[3]){
     int move[4];
 
     printf("Player %c's turn. Enter the row and column of the first dot and second dot:\n",p);
+    int n;
+    // need to 1. Make sure input is valid here and 
+    // 2. if input is invalid, scanf will not flush it automatically. So we need to flush it manually:
     for(int i = 0; i < 4; i++){
-        scanf("%d", &move[i]);
+        n = scanf("%d", &move[i]);
+        if ( n != 1){
+            printf("Invalid input, please try again\n");
+            
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+            return 1;
+            //this will keep reading forward until the buffer is empty
+            // since getchar() and scanf both use stdin, it will get flushed
+        }
+    }
+    //now make sure input is valid
+    int v = validateInput(&move);
+
+    if(v == -1){
+        printf("Invalid input, please try again\n");
+        return 1;
+    }
+
+    coord[0] = v;
+    coord[1] = move[0];
+    coord[2] = move[1];
+
+    if(v){    
+        if (vlines[move[0]][move[1]] == '\0'){ //check a line does not already occupy this space
+            vlines[move[0]][move[1]] = '|';
+            return 0;
+        } else {
+            printf("There is already a line here, please try again.\n");
+            return 1;
+        }
+    } else {    
+        if (hlines[move[0]][move[1]] == '\0'){ //check a line does not already occupy this space
+            hlines[move[0]][move[1]] = '-';
+            return 0;
+        } else {
+            printf("There is already a line here, please try again.\n");
+            return 1;
+        }
     }
 }
--- main.c
+++ main.c
@@ -5,18 +5,29 @@
 
 
 int main(){
+    char players[2] = {'A','B'};
+    int scores[2];
+    int point[3]= {0};
     char vlines[HEIGHT][LENGTH+1]={0};
     char hlines[HEIGHT+1][LENGTH]={0}; 
     char owned[HEIGHT][LENGTH]={0};
+    
 
-    vlines[2][3]='|';
-    vlines[2][4]='|';
-    owned[2][3]='A';
-    hlines[2][2] ='-';
-    hlines[3][2] = '-';
-   
+    int i= 0;
+    while(scores[0] + scores[1] < HEIGHT*LENGTH){ // while there are unclaimed squares
+        displayBoard(vlines, hlines, owned);
+        while (playerMove(players[i%2], vlines, hlines, point)){ // keep playing until a valid input is registered
+            continue; 
+        }
+        //Need to add: if a player completes a square, repeat their turn
 
-    displayBoard(vlines, hlines, owned);
+
+        i++; //incement to move to the next player
+   }
+
+
+
+
 
     return 0;
 }
