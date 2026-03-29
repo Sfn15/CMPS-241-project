9c3ed96e6543049eab4f8ee06695281c467da24f
Sfn15 <sfn15@mail.aub.edu>
2026-03-29 22:03:02 +0300

    fixed the gameplay loop in the main function and uninitalized scores array. enjoy the game!

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 int main(){
     char players[2] = {'A','B'};
-    int scores[2];
+    int scores[2]={0,0};
     int point[3]= {0};
     char vlines[HEIGHT][LENGTH+1]={0};
     char hlines[HEIGHT+1][LENGTH]={0}; 
@@ -21,11 +21,20 @@ int main(){
         }
         
         int boxesClaimed = boxCheck(players[i%2], vlines, hlines, owned, point);
-        scores[i%2] += boxesClaimed;
+        scores[i%2] += 10*boxesClaimed;
+        printf("Updated score : A: %d, B: %d\n",scores[0],scores[1]);
         
         if(boxesClaimed == 0) {
             i++;
         }
+   }
+
+   if(scores[0] > scores[1]){
+    printf("Player %c wins with a score of %d!",players[0],scores[0]);
+   } else if (scores[0] == scores[1]){
+    printf("The game is a tie with both players having a score of %d", (HEIGHT*LENGTH)/2);
+   } else {
+    printf("Player %c wins with a score of %d!",players[1],scores[1]);
    }
 
 
