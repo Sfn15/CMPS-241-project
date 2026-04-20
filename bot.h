b7190584f2f9d4f267f677f4952652de6d130b63
Hassan-Jf <140070608+Hassan-Jf@users.noreply.github.com>
2026-04-20 12:51:52 +0300

    removed coord[3] from bot.h too

--- bot.h
+++ bot.h
@@ -8,7 +8,7 @@ struct chain{
     int endpoint;
 };
 
-int moveA(char p, char vlines[HEIGHT][LENGTH+1], char hlines[HEIGHT+1][LENGTH], char owned[HEIGHT][LENGTH],int coord[3]);
+int moveA(char p, char vlines[HEIGHT][LENGTH+1], char hlines[HEIGHT+1][LENGTH], char owned[HEIGHT][LENGTH]);
 
 void findChains(char vlines[HEIGHT][LENGTH+1], char hlines[HEIGHT+1][LENGTH], char owned[HEIGHT][LENGTH], struct chain chains[HEIGHT*LENGTH]);
 
